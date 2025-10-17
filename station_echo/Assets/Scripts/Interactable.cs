d3aef0acda8867ac52ccbcec4318e98c876c4cc9
nazar vasyltsiv <vasylnaz@cvut.cz>
2025-10-17 16:54:12 +0200

    Added the ability to pick up objects

---
+++ station_echo/Assets/Scripts/Interactable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Used to determine wheather the player can interact with this object (pick it up / press it)
+}
---
+++ station_echo/Assets/Scripts/UpdateInteractionArea.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class UpdateInteractionArea : MonoBehaviour
+{
+    private PlayerInteractionLogic playerInteractionLogic;
+    void Start()
+    {
+        playerInteractionLogic = GetComponentInParent<PlayerInteractionLogic>();
+    }
+
+    void Update()
+    {
+        checkUnavailableInteractions();
+    }
+
+    private void checkUnavailableInteractions()
+    {
+        if (playerInteractionLogic.unavailableInteractions.Count == 0) return;
+        for (int i = playerInteractionLogic.unavailableInteractions.Count - 1; i >= 0; i--)
+        {
+            GameObject gameObject = playerInteractionLogic.unavailableInteractions[i];
+            if (!CheckLineOfSight(gameObject))
+            {
+                playerInteractionLogic.availableInteractions.Add(gameObject);
+                playerInteractionLogic.unavailableInteractions.RemoveAt(i);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.GetComponent<Interactable>()) return;
+
+        if (CheckLineOfSight(other.gameObject))
+        {
+            playerInteractionLogic.unavailableInteractions.Add(other.gameObject);
+        }
+        else
+        {
+            playerInteractionLogic.availableInteractions.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.GetComponent<Interactable>()) return;
+
+        if (!playerInteractionLogic.availableInteractions.Remove(other.gameObject))
+        {
+            playerInteractionLogic.unavailableInteractions.Remove(other.gameObject);
+        }
+    }
+
+    // false - if clear, true - not clear
+    private bool CheckLineOfSight(GameObject targetObject) 
+    {
+        Vector3 startPoint = playerInteractionLogic.transform.position;
+        Vector3 endPoint = targetObject.gameObject.transform.position;
+
+        Vector3 direction = (endPoint - startPoint).normalized;
+        float distance = Vector3.Distance(startPoint, endPoint);
+        RaycastHit hit;
+       
+        if (Physics.Raycast(startPoint, direction, out hit, distance, playerInteractionLogic.layerMask))
+        {
+            if (hit.transform == targetObject.gameObject.transform)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+           return false;
+        }
+    }
+}

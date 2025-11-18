015c64ea5f4f64c748a822509328c089a7eccd57
Ihor Holub <holubihor@gmail.com>
2025-11-18 16:34:45 +0100

    ReverseGravity script created

---
+++ station_echo/Assets/Scripts/Objects Logic/DispenserLogic.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class DispensorLogic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    enum DispenseType
+    {
+        OnDemand,
+        OnDestroy,
+        Mixed
+    }
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

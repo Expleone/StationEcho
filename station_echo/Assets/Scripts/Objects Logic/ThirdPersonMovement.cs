ec7450a67484c5926f1a7d462d82ae17a7875686
vasylnaz <vasylnaz@cvut.cz>
2025-12-01 21:22:28 +0100

    Level1 underway

--- station_echo/Assets/Scripts/Objects Logic/ThirdPersonMovement.cs
+++ station_echo/Assets/Scripts/Objects Logic/ThirdPersonMovement.cs
@@ -158,11 +158,6 @@ public class ThirdPersonMovement : MonoBehaviour
         animator = mesh.GetComponent<Animator>();
         swapper = mesh.GetComponentInChildren<MaterialSwapper>();
         wasRunning = false;
-
-        if (cameraTargetPoint == null)
-        {
-            Debug.LogWarning("CameraTargetPoint does not exist!");
-        }
     }
 
     
